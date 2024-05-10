dda06ef147003b90ddc9c4e98b822dd39387106b
Rangel <rangelplachkov1@gmail.com>
2024-05-10 19:11:02 +0300

    Initial commit

---
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/PopcornPalsApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.popcornpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornPalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopcornPalsApplication.class, args);
+    }
+
+}
---
+++ PopcornPals-Server/src/test/java/bg/fmi/popcornpals/PopcornPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.fmi.popcornpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornPalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
