d976b1ba616075335be6a94ea9becb19127d4d91
Rangel <rangelplachkov1@gmail.com>
2024-05-18 11:54:27 +0300

    Update StringSize.java

--- PopcornPals-Server/src/main/java/bg/fmi/popcornpals/util/StringSize.java
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/util/StringSize.java
@@ -1,8 +1,8 @@
 package bg.fmi.popcornpals.util;
 
 public interface StringSize {
-    int TITLE_MAX = 255;
-    int DESCRIPTION_MAX = 255;
-    int REVIEW_MAX = 255;
-    int NAME_MAX = 255;
+    int TITLE_MAX = 64;
+    int DESCRIPTION_MAX = 2048;
+    int REVIEW_MAX = 2048;
+    int NAME_MAX = 64;
 }
