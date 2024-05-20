81c75767af6739e2d17b0f1c5c5c7a0d775e4b31
Rangel <rangelplachkov1@gmail.com>
2024-05-20 17:13:21 +0300

    create studio mapper
    add dependecies for mapping

--- PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/StudioController.java
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/StudioController.java
@@ -1,7 +1,11 @@
 package bg.fmi.popcornpals.controller;
 
+import bg.fmi.popcornpals.dto.StudioDTO;
 import bg.fmi.popcornpals.service.StudioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,9 @@ public class StudioController {
     @Autowired
     private StudioService studioService;
 
+    @PostMapping("/create")
+    public void createStudio(@Valid @RequestBody StudioDTO studio) {
+        //studioService.createStudio();
+    }
 
 }
---
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/mapper/StudioMapper.java
@@ -0,0 +1,23 @@
+package bg.fmi.popcornpals.mapper;
+
+import bg.fmi.popcornpals.dto.StudioDTO;
+import bg.fmi.popcornpals.model.Studio;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface StudioMapper {
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "foundingDate", target = "foundingDate")
+    StudioDTO toDTO(Studio studio);
+
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "foundingDate", target = "foundingDate")
+    Studio fromDTO(StudioDTO studioDTO);
+
+
+}
