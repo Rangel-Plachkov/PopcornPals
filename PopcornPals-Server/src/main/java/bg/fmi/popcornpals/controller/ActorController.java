09e9f497813639180dda668141c8bf6d58a2afff
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2024-05-24 14:26:03 +0300

    changed endpoints name

--- PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/ActorController.java
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/ActorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/actors/")
+@RequestMapping(path = "api/actors/")
 public class ActorController {
     private final ActorService actorService;
 
