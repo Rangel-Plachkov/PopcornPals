b533dd88a0875693801f7236ef718a61275fe5ff
Rangel <rangelplachkov1@gmail.com>
2024-05-29 11:14:24 +0300

    Update ReviewController.java

--- PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/ReviewController.java
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package bg.fmi.popcornpals.controller;
 
+import bg.fmi.popcornpals.mapper.ReviewMapper;
 import bg.fmi.popcornpals.service.ReviewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
+
+    @Autowired
+    private ReviewMapper reviewMapper;
 
 
 
