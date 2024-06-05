570d9eed592fe8ca9794ce619a48f11e64504a50
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2024-06-05 17:26:06 +0300

    added studio not found exception

---
+++ PopcornPals-Server/src/main/java/bg/fmi/popcornpals/exception/StudioNotFoundException.java
@@ -0,0 +1,16 @@
+package bg.fmi.popcornpals.exception;
+
+public class StudioNotFoundException extends NotFoundException {
+    public StudioNotFoundException() {
+        super("Studio: Studio not found");
+    }
+    public StudioNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public StudioNotFoundException(String message) {
+        super(message);
+    }
+    public StudioNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
